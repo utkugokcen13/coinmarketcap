85bd16fea0f52a73a1f2d14a7f3e44886bbb9593
Utku Gökçen <utku.gokcen13@gmail.com>
2021-03-06 11:30:45 +0300

    We get the data with API

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
 import json
 
+api_request = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/"
+                           "latest?start=1&limit=5&convert=USD&CMC_"
+                           "PRO_API_KEY=0ac7a2fa-1593-4cb0-b0f4-5b15f8889bca")
+
+result = json.loads(api_request.content)
+print(result)
